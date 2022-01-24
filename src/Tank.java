a96cbf1b1d7b7a9fe094f93d31ea55df8c5e8068
JannaZZZ <w-e_like@mail.ru>
2022-01-24 17:39:21 +0400

    hhhh

---
+++ src/Tank.java
@@ -0,0 +1,22 @@
+public class Tank {
+    int Position = 0;
+    void goForward(int hod){
+        Position+= hod;
+    }
+    void printPosition(){
+        System.out.println("The Tank is at " + Position + " now");
+    }
+    void goBackward(int hod){
+        Position-= hod;
+    }
+
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.goForward(10);
+        tank.printPosition();
+        tank.goForward(50);
+        tank.printPosition();
+        tank.goBackward(100);
+        tank.printPosition();
+    }
+}
